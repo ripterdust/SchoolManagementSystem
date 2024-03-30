07c2e4cb728e786054b6be6bf7a5f41f2491f0d1
ripterdust <bryantello2010@hotmail.com>
2024-03-30 02:23:14 -0600

    Added base controler and courses logic

--- server/app/controllers/base_controller.rb
+++ server/app/controllers/base_controller.rb
@@ -1,2 +1,35 @@
+# Base controller, It handles the basic crud operations -> index, 
 class BaseController < ApplicationController
+
+    model = nil
+    
+    #GET -> fetch all records
+    def index 
+        return base_response("Index")
+    end
+
+    def create 
+        return base_response("Create")
+    end
+
+    def show 
+        return base_response("Show")
+        
+    end
+
+    def update
+        return base_response("Update")
+    end
+
+    def destroy
+        return base_response("Destroy")
+    end
+
+    private 
+    def base_response(data)
+        render json: {
+            data: data
+        }, status: :ok
+    end
+
 end
---
+++ server/app/controllers/courses_controller.rb
@@ -0,0 +1,2 @@
+class CoursesController < BaseController
+end
