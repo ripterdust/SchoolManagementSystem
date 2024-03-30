fc19dfdc9641887ba080cd12df8c695d02312724
ripterdust <bryantello2010@hotmail.com>
2024-03-30 01:06:49 -0600

    Added authentication methods

--- server/app/controllers/home_controller.rb
+++ server/app/controllers/home_controller.rb
@@ -1,9 +1,15 @@
 class HomeController < ApplicationController
+    skip_before_action :authenticate_request, only: [:index]
+    
     def index
         response = {
-            message: "Hello world"
+            message: "School management routes",
+            login_url: {
+                path: "/auth/login",
+                method: "POST"
+            }
         }
 
-        render json: response
+        render json: response, status: :ok
     end
 end
