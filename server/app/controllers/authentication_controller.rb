9f85faf818cbb2109809c636ba641fe455e96328
ripterdust <bryantello2010@hotmail.com>
2024-03-30 00:40:15 -0600

    Added routes

--- server/app/controllers/authentication_controller.rb
+++ server/app/controllers/authentication_controller.rb
@@ -6,14 +6,24 @@ class AuthenticationController < ApplicationController
         
         isAuthenticated = @user&.authenticate(params[:password])
 
+        puts isAuthenticated
+
         if !isAuthenticated
             render json: { error: ["Invalid username or password"] }, status: :unauthorized            
             return 
         end
-        
+
         token = Jsonwebtoken.jwt_encode(user_id: @user.id)
             
         return render json: { data: token }, status: :ok
     end
 
+    #POST /auth/register
+
+    def register
+
+        token = "Hola"
+        return render json: {data: token}, status: :ok
+        
+    end
 end
--- server/config/routes.rb
+++ server/config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   resource :users
+
+  post '/auth/login', to: "authentication#login"
 end
