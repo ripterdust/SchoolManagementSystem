f681b6322d779ac27495cc0b868ddd20085f00f5
ripterdust <bryantello2010@hotmail.com>
2024-03-30 02:24:10 -0600

    Fixed recourses files

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :users
-  resources :courses
+  resources :users, :courses
+  
   get '/', to: "home#index"
   post '/auth/login', to: "authentication#login"
   post '/auth/register', to: "authentication#register"
