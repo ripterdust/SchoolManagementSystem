1173ba9f1594c95ee9c202de56ba3989f545892d
ripterdust <bryantello2010@hotmail.com>
2024-03-31 01:04:42 -0600

    Removed angular material

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
--- client/src/app/skeletons/auth-skeleton/auth-skeleton.component.ts
+++ client/src/app/skeletons/auth-skeleton/auth-skeleton.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-auth-skeleton',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatCardModule],
   templateUrl: './auth-skeleton.component.html',
 })
 export class AuthSkeletonComponent {}
