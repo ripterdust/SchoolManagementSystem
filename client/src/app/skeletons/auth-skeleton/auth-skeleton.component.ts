b3079742076474c7eb767306aa261a3039b92332
ripterdust <bryantello2010@hotmail.com>
2024-03-31 00:46:31 -0600

    Fixed components

--- client/src/app/skeletons/auth-skeleton/auth-skeleton.component.ts
+++ client/src/app/skeletons/auth-skeleton/auth-skeleton.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CheckboxModule } from 'primeng/checkbox';
 
 @Component({
   selector: 'app-auth-skeleton',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CheckboxModule],
   templateUrl: './auth-skeleton.component.html',
 })
 export class AuthSkeletonComponent {}
