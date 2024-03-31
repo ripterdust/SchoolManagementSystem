a0163384c0766c49d754ea95dd38feb7480808ca
ripterdust <bryantello2010@hotmail.com>
2024-03-31 00:46:46 -0600

    Removed prime ng

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { StyleClassModule } from 'primeng/styleclass';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, StyleClassModule],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
--- client/src/app/skeletons/auth-skeleton/auth-skeleton.component.ts
+++ client/src/app/skeletons/auth-skeleton/auth-skeleton.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { CheckboxModule } from 'primeng/checkbox';
 
 @Component({
   selector: 'app-auth-skeleton',
   standalone: true,
-  imports: [RouterOutlet, CheckboxModule],
+  imports: [RouterOutlet],
   templateUrl: './auth-skeleton.component.html',
 })
 export class AuthSkeletonComponent {}
